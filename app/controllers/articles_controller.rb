fe1e299b84e37a9dab7f0a86f7a9ff9d125f6b01
Peter Aarestad <aarestad@gmail.com>
2020-03-23 19:53:46 -0500

    edit/delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,12 +6,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new
-    @article = Article.new
-  end
-
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
   end
 
   def edit
@@ -26,6 +26,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
