daf9fb6308fc087469392a277dd16f2cf4f78b3c
Peter Aarestad <aarestad@gmail.com>
2020-03-24 21:55:41 -0500

    chapter 9

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@
 
 # The article controller
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
