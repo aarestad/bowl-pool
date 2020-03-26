261a51ecf79a3aba60136f24ddc5c150bda42845
Peter Aarestad <aarestad@gmail.com>
2020-03-25 21:39:23 -0500

    neeeat

--- app/models/bowl_matchup.rb
+++ app/models/bowl_matchup.rb
@@ -2,6 +2,6 @@
 
 class BowlMatchup < ApplicationRecord
   belongs_to :bowl_game
-  belongs_to :away_team
-  belongs_to :home_team
+  belongs_to :away_team, class_name: 'Team'
+  belongs_to :home_team, class_name: 'Team'
 end
