2d6c395367a8bf82c0f92e0d642894b488017d17
Peter Aarestad <aarestad@gmail.com>
2020-03-25 21:20:11 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-teams = Team.create([
-                        {name: 'Wisconsin', abbrev: 'WIS'},
-                    ])
+Team.create([
+                {name: 'Wisconsin', abbrev: 'WIS'},
+                {name: 'Oregon', abbrev: 'ORE'},
+            ])
